efb5f660302b0431fa392a8e07758a82601118cf
moonsettler <moonsettler@protonmail.com>
2021-11-18 20:06:25 +0100

    Update error.rs
    first attempt at making better Display for CompilationError...

--- sapio/src/contract/error.rs
+++ sapio/src/contract/error.rs
@@ -112,7 +112,48 @@ impl From<ObjectError> for CompilationError {
 
 impl fmt::Display for CompilationError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
+        match &self {
+            // Unspecified Error -- but we should stop compiling
+            CompilationError::TerminateCompilation => f.write_str("Compile Error: Unspecified!"),
+            // Fee Specification Error
+            CompilationError::MinFeerateError => f.write_str("Compile Error: Minimum Fee Rate!"),
+            // Error when ContextPath has already been used.
+            CompilationError::ContexPathAlreadyDerived => f.write_str("Compile Error: ContextPath has already been used!"),
+            // Error when ContextPath attempted
+            CompilationError::InvalidPathName => f.write_str("Compile Error: InvalidPathName!"),
+            // Other Error for Fragment Format
+            CompilationError::PathFragmentError(e) => write!(f, "Compile Error; Path Fragment: {:?}", e),
+            // Error when a `ThenFunc` returns no Templates.
+            CompilationError::MissingTemplates => f.write_str("Compile Error: `ThenFunc` returns no Templates!"),
+            // Error if a Policy is empty
+            CompilationError::EmptyPolicy => f.write_str("Compile Error: Policy is empty!"),
+            // Error if a contract does not have sufficient funds available
+            CompilationError::OutOfFunds => f.write_str("Compile Error: Contract does not have sufficient funds available!"),
+            // Error if a CheckSequenceVerify clause is incompatible with the sequence already set.
+            // E.g., blocks and time
+            CompilationError::IncompatibleSequence => f.write_str("Compile Error: CheckSequenceVerify clause is incompatible with the sequence already set!"),
+            // Error if a CheckLockTime clause is incompatible with the locktime already set.
+            // E.g., blocks and time
+            CompilationError::IncompatibleLockTime => f.write_str("Compile Error: CheckLockTime clause is incompatible with the locktime already set!"),
+            // Error if a sequence at index j >= inputs.len() is attempted to be set
+            CompilationError::NoSuchSequence => f.write_str("Compile Error: No such sequence!"),
+            // Error if parsing an Amount failed
+            CompilationError::ParseAmountError(e) => write!(f, "Compile Error; Parse Amount: {:?}", e),
+            // Error from the Policy Compiler
+            CompilationError::Miniscript(e) => write!(f, "Compile Error; Miniscript Policy: {:?}", e),
+            // Error from the miniscript system
+            CompilationError::MiniscriptE(e) =>  write!(f, "Compile Error; Miniscript: {:?}", e),
+            // Error with a Timelock
+            CompilationError::TimeLockError(e) => write!(f, "Compile Error; Time Lock: {:?}", e),
+            // Error creating an object,
+            CompilationError::CompiledObjectError(e) => write!(f, "Compile Error; Object: {:?}", e),
+            // Failure in conditional compilation logic
+            CompilationError::ConditionalCompilationFailed(_ls) => f.write_str("Compilation Error: Failure in conditional compilation logic!"),
+            // Error from the Effects system
+            CompilationError::EffectDBError(e) => write!(f, "Compile Error; Effect DB: {:?}", e),
+            // Unknown Error type -- either from a user or from some unhandled dependency
+            CompilationError::Custom(e) => write!(f, "Compile Error: {:?}", e)
+        }
     }
 }
 
